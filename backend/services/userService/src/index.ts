27decd9aa1fd17bdf47c10c539362ddb0db7fc30
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-12 18:58:51 +0530

    chore: Add backend files and dependencies for user service

---
+++ backend/services/userService/src/index.ts
@@ -0,0 +1,22 @@
+
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import logger from 'morgan';
+import cors from 'cors'
+import router from './routes/router';
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+app.use(logger('dev'));
+app.use(express.json()); 
+app.use(cors())
+
+app.use('/', router)
+
+
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
