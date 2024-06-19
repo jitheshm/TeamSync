582092a7d102302717a1c338d050e42f5d1815b1
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-19 16:26:25 +0530

    authentication service created with database and kafka connection

---
+++ backend/services/authenticationService/src/config/kafka/KafkaConnection.ts
@@ -0,0 +1,51 @@
+import { Consumer, Kafka, Producer } from "kafkajs"
+import { IKafkaConnection } from "../../interfaces/IKafkaConnection";
+// import { IKafkaConnection } from "../../interfaces/IKafkaConnection";
+
+const kafkaInstance = new Kafka({
+    brokers: ["redpanda-0:9092"]
+
+})
+
+export class KafkaConnection implements IKafkaConnection {
+
+    private static producer: Producer;
+    private consumer?: Consumer
+
+    // constructor(){}
+
+    async getProducerInstance() {
+        try {
+
+            if (!KafkaConnection.producer) {
+                KafkaConnection.producer = kafkaInstance.producer()
+                await KafkaConnection.producer.connect()
+                console.log('Producer connected');
+
+            }
+            return KafkaConnection.producer
+
+        } catch (error) {
+            console.log('Error in KafkaConnection getProducerInstance method');
+
+            console.log(error);
+            throw error
+
+        }
+    }
+
+    async getConsumerInstance(groupId: string) {
+        try {
+
+            this.consumer = kafkaInstance.consumer({ groupId: groupId })
+            await this.consumer.connect()
+
+            return this.consumer
+        } catch (error) {
+            console.log(error);
+            throw error
+
+        }
+
+    }
+}
---
+++ backend/services/authenticationService/src/index.ts
@@ -0,0 +1,28 @@
+
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import logger from 'morgan';
+import cors from 'cors'
+import connect from './config/db/connect';
+// import { connectConsumers } from './events/kafka/consumerStart';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 3002;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors())
+connect().then(() => {
+  // connectConsumers()
+})
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
