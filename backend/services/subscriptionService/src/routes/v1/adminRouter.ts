aab81347e6e7928db27e7b307c7b60528a68cc6b
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-21 17:58:55 +0530

    plan delete api added in subscription service

--- backend/services/subscriptionService/src/routes/v1/adminRouter.ts
+++ backend/services/subscriptionService/src/routes/v1/adminRouter.ts
@@ -4,11 +4,13 @@ import planValidator from "../../validators/planValidator";
 import planController from "../../controllers/v1/planController";
 import adminAuth from "../../middlewares/adminAuth";
 import updatePlanController from "../../controllers/v1/updatePlanController";
+import deletePlanController from "../../controllers/v1/deletePlanController";
 
 
 const router = Router();
 
 router.post('/subscription-plans', adminAuth, checkSchema(planValidator()), planController)
 router.put('/subscription-plans/:planId', adminAuth, checkSchema(planValidator()), updatePlanController)
+router.delete('/subscription-plans/:planId', adminAuth, deletePlanController)
 
 export default router
