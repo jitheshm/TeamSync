9f6038056c3e25921af8f86ad4e1a5f42dc1683e
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-24 19:05:41 +0530

    navigation link added in forget password page

--- frontend/src/components/Login/ForgotPassword.tsx
+++ frontend/src/components/Login/ForgotPassword.tsx
@@ -1,5 +1,6 @@
 import { forgetPassword } from '@/api/authService/auth';
 import instance from '@/axios';
+import Link from 'next/link';
 import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { z } from 'zod';
 
@@ -90,7 +91,7 @@ const ForgotPassword: React.FC<ForgotPasswordProps> = ({ setEmail, setOtpVisible
                 </div>
                 <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Send OTP</button>
                 <div className="text-sm font-medium text-gray-500 dark:text-dark-300 text-center">
-                    Not registered? <a href="/signup" className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                    Not registered? <Link href="/signup" className="text-blue-700 hover:underline dark:text-blue-500">Create account</Link>
                 </div>
             </form>
         </div>
