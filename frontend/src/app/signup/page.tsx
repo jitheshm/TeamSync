7a9438dc077961ac872678193226faf0b584ef4c
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-24 15:16:03 +0530

    token verification added in signup page

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -1,16 +1,48 @@
+"use client"
 import SignUp from '@/components/Login/SignUp'
 import LoginLanding from '@/components/Login'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import UserLayout from '@/components/Layout/UserLayout'
+import { useDispatch } from 'react-redux'
+import { useRouter } from 'next/navigation'
+import { verifyToken } from '@/api/authService/auth'
+import { verify } from '@/features/user/userSlice'
+import Cookies from 'js-cookie'
+import Loading from '@/components/Loading/Loading'
 
-function page() {
+function Page() {
+    const [loading, setLoading] = useState(true)
+    const dispatch = useDispatch()
+    const router = useRouter()
+    useEffect(() => {
+        const token = Cookies.get('team-sync-user-token')
+        if (token) {
+            verifyToken(token).then((data) => {
+                dispatch(verify({ name: data.user }))
+                router.push('/')
+
+            }).catch((error) => {
+                console.log(error);
+
+                setLoading(false)
+                Cookies.remove('team-sync-user-token')
+
+            })
+        } else {
+            setLoading(false)
+        }
+
+    }, [])
     return (
         <UserLayout>
-            <LoginLanding>
-                <SignUp />
-            </LoginLanding>
+            {
+                loading ? <Loading /> :
+                    <LoginLanding>
+                        <SignUp />
+                    </LoginLanding>
+            }
         </UserLayout>
     )
 }
 
-export default page
+export default Page
