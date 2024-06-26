cadd78a9ea3d30cea6d74d03228fce97db108077
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-27 01:57:28 +0530

    authentication added in navbar, sample home page added

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,12 @@
+import UserLayout from "@/components/Layout/UserLayout";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div></div>
+    <UserLayout>
+      <div className="h-screen bg-white pt-24">
+        <h4 className="text-gray-950"> Welcome to home page</h4>
+      </div>
+    </UserLayout>
   );
 }
