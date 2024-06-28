e7fc9ed2f2d2369cbaf9e86c390dfe66dcbd0acc
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-28 14:32:05 +0530

    user detaisl page added in admin panel

---
+++ frontend/src/app/admin/dashboard/users/[userId]/page.tsx
@@ -0,0 +1,11 @@
+import UserDetails from '@/components/UserDetails/UserDetails'
+import React from 'react'
+
+function page() {
+    return (
+       <UserDetails/>
+
+    )
+}
+
+export default page
---
+++ frontend/src/components/UserDetails/UserDetails.tsx
@@ -0,0 +1,99 @@
+import React from 'react'
+
+function UserDetails() {
+    return (
+        <div className="container mx-auto my-5 p-5">
+            <div className="md:flex no-wrap md:-mx-2 ">
+                {/* Left Side */}
+                <div className="w-full md:w-3/12 md:mx-2">
+                    {/* Profile Card */}
+                    <div className="bg-gray-700 p-3 border-t-4 border-green-400">
+
+                        <h1 className="text-gray-100 font-bold text-xl leading-8 my-1">Jane Doe</h1>
+                        <h3 className="text-gray-100 font-lg text-semibold leading-6">Owner at Her Company Inc.</h3>
+                        <ul className="bg-gray-100 text-gray-600 hover:text-gray-700 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
+                            <li className="flex items-center py-3">
+                                <span>Status</span>
+                                <span className="ml-auto"><span className="bg-green-500 py-1 px-2 rounded text-white text-sm">Active</span></span>
+                            </li>
+                            <li className="flex items-center py-3">
+                                <span>Member since</span>
+                                <span className="ml-auto">Nov 07, 2016</span>
+                            </li>
+                        </ul>
+                    </div>
+                    {/* End of profile card */}
+                    <div className="my-4" />
+                    {/* Friends card */}
+
+                    <div className="bg-gray-700 p-3 hover:shadow">
+                        <p>Subscription Details</p>
+                    </div>
+                    {/* End of friends card */}
+                </div>
+                {/* Right Side */}
+                <div className="w-full md:w-9/12 mx-2 h-64">
+                    {/* Profile tab */}
+                    {/* About Section */}
+                    <div className="bg-gray-700 p-3 shadow-sm rounded-sm">
+                        <div className="flex items-center space-x-2 font-semibold text-gray-100 mt-4 leading-8">
+                            <span clas="text-green-500">
+                                <svg className="h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
+                                </svg>
+                            </span>
+                            <span className="tracking-wide">About</span>
+                        </div>
+                        <div className="text-gray-100">
+                            <div className="grid md:grid-cols-2 text-sm">
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">First Name</div>
+                                    <div className="px-4 py-2">Jane</div>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">Last Name</div>
+                                    <div className="px-4 py-2">Doe</div>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">Gender</div>
+                                    <div className="px-4 py-2">Female</div>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">Contact No.</div>
+                                    <div className="px-4 py-2">+11 998001001</div>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">Current Address</div>
+                                    <div className="px-4 py-2">Beech Creek, PA, Pennsylvania</div>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">Permanant Address</div>
+                                    <div className="px-4 py-2">Arlington Heights, IL, Illinois</div>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">Email.</div>
+                                    <div className="px-4 py-2">
+                                        <a className="text-blue-300" href="mailto:jane@example.com">jane@example.com</a>
+                                    </div>
+                                </div>
+                                <div className="grid grid-cols-2">
+                                    <div className="px-4 py-2 font-semibold">Birthday</div>
+                                    <div className="px-4 py-2">Feb 06, 1998</div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    {/* End of about section */}
+                    <div className="my-4" />
+                    {/* Experience and education */}
+                    <div className="bg-gray-700 p-3 shadow-sm rounded-sm">
+                        <p>Transactions</p>
+
+                        {/* End of Experience and education grid */}
+                    </div>
+                </div></div></div>
+    )
+}
+
+export default UserDetails
