72ab8a373c69c5c20d81210fd947a5b4377fe2f2
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-28 15:21:44 +0530

    user registration api added for admin in user service

---
+++ backend/services/userService/src/controllers/v1/registerUserController.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express";
+import UserRepository from "../../repository/implementations/UserRepository";
+import { IUsers } from "../../entities/UserEntity";
+import bcrypt from "../../utils/bcrypt";
+import { validationResult } from "express-validator";
+import { KafkaConnection } from "../../config/kafka/KafkaConnection";
+import UserProducer from "../../events/kafka/producers/UserProducer";
+import { IUserRepository } from "../../repository/interface/IUserRepository";
+import { IKafkaConnection } from "../../interfaces/IKafkaConnection";
+import Stripe from "stripe";
+
+let userRepo: IUserRepository = new UserRepository()
+let kafkaConnection: IKafkaConnection = new KafkaConnection()
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+
+
+export default async (req: Request, res: Response) => {
+
+    try {
+
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.status(400).json({ errors: result.array() });
+        }
+
+        let user: IUsers = req.body;
+        let userExist = await userRepo.fetchUserByEmail(user.email)
+        if (userExist) {
+            return res.status(409).json({ error: "Email address already exists." })
+        }
+
+
+        user.password = bcrypt(user.password)
+        user.user_id = '#user' + new Date().getTime() + Math.floor(Math.random() * 1000)
+
+        const customer = await stripe.customers.create({
+            email: user.email,
+            name: user.first_name,
+
+
+        });
+
+        user.stripe_customer_id = customer.id
+        user.is_verified = true
+
+        const newUser = await userRepo.create(user)
+
+
+        // send message to kafka
+
+        let producer = await kafkaConnection.getProducerInstance()
+        let userProducer = new UserProducer(producer, 'main', 'users')
+        userProducer.sendMessage('create', newUser)
+
+
+
+        res.status(201).json({ message: "user registered successfully" })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "An unexpected error occurred. Please try again later." })
+    }
+}
--- backend/services/userService/src/routes/v1/adminRouter.ts
+++ backend/services/userService/src/routes/v1/adminRouter.ts
@@ -5,12 +5,14 @@ import blockUserController from "../../controllers/v1/blockUserController";
 import unBlockUserController from "../../controllers/v1/unBlockUserController";
 import deleteUsersController from "../../controllers/v1/deleteUsersController";
 import fetchSpecificUserController from "../../controllers/v1/fetchSpecificUserController";
+import registerUserController from "../../controllers/v1/registerUserController";
 
 
 
 const router = Router();
 
 router.get('/users', adminAuth, fetchUsersController)
+router.post('/users', adminAuth, registerUserController)
 router.patch('/users/block/:userId', adminAuth, blockUserController)
 router.patch('/users/unblock/:userId', adminAuth, unBlockUserController)
 router.delete('/users/:userId', adminAuth, deleteUsersController)
