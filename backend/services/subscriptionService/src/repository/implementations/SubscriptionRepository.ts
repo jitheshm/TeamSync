cf926e9bc85bbe03676949f41d00a559e545d625
Jithesh M <jitheshmjithooz@gmail.com>
2024-06-28 23:44:13 +0530

    chore: Refactor SubscriptionRepository to include additional data in findSubscriptionByUserId method

--- backend/services/subscriptionService/src/repository/implementations/SubscriptionRepository.ts
+++ backend/services/subscriptionService/src/repository/implementations/SubscriptionRepository.ts
@@ -42,8 +42,70 @@ export default class SubscriptionRepository implements ISubscriptionRepository {
     async findSubscriptionByUserId(userId: mongoose.Types.ObjectId) {
         try {
             const SubscriptionModel = switchDb<ISubscriptions>(`${process.env.SERVICE}_main`, 'subscriptions')
-            const res = await SubscriptionModel.findOne({ user_id: userId })
-            return res
+            const res = await SubscriptionModel.aggregate([
+                {
+                    $match:{
+                        user_id:userId
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'plans',
+                        localField: 'plan_id',
+                        foreignField: 'stripe_plan_id',
+                        as: 'plan'
+                    }
+                },
+                {
+                    $lookup: {
+                        from: 'tenants',
+                        localField: 'tenant_id',
+                        foreignField: '_id',
+                        as: 'tenant'
+                    }
+                },
+                {
+                    $unwind: '$plan'
+                },
+                // {
+                //     $unwind: '$tenant'
+                // },
+                {
+                    $project: {
+                        _id: 1,
+                        subscription_id: 1,
+                        stripe_subscription_id: 1,
+                        tenant_id: 1,
+                        plan_id: 1,
+                        user_id: 1,
+                        stripe_latest_invoice: 1,
+                        stripe_customer_id: 1,
+                        status: 1,
+                        payment_method: 1,
+                        renewal_date: 1,
+                        plan: {
+                            _id: 1,
+                            name: 1,
+                            price: 1,
+                            description: 1,
+                            currency: 1,
+                            interval: 1,
+                            trial_period_days: 1,
+                            status: 1
+                        },
+                        tenant: {
+                            _id: 1,
+                            name: 1,
+                            description: 1,
+                            status: 1
+                        }
+                    }
+                
+                }
+            ]).exec()
+            console.log(res);
+            
+            return res[0]
         } catch (error) {
             console.log('Error in SubscriptionRepository findSubscriptionByUserId method');
 
