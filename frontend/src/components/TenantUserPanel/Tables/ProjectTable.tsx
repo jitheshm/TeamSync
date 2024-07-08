5d2dbc69c90b27c3bfc53ee7c382f713c81be1d0
Jithesh M <jitheshmjithooz@gmail.com>
2024-07-08 18:09:03 +0530

    search updated

--- frontend/src/components/TenantUserPanel/Tables/ProjectTable.tsx
+++ frontend/src/components/TenantUserPanel/Tables/ProjectTable.tsx
@@ -90,7 +90,7 @@ const ProjectTable = ({ role }: { role: string }) => {
                             <path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd" />
                         </svg>
                         <input
-                            className="bg-gray-50 outline-none ml-1 block"
+                            className="bg-gray-50 outline-none text-gray-950 ml-1 block"
                             type="text"
                             name="search"
                             id="search"
