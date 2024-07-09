cd305f5df13ca8b55673e4503499f481cef37425
Jithesh M <jitheshmjithooz@gmail.com>
2024-07-09 19:16:29 +0530

    communication service created

---
+++ backend/services/communicationService/src/index.ts
@@ -0,0 +1,33 @@
+
+import express, { Application } from 'express';
+import dotenv from 'dotenv';
+import logger from 'morgan';
+import cors from 'cors'
+import connect from './config/db/connect';
+// import { connectConsumers } from './events/kafka/consumerStart';
+// import router from './routes/router';
+
+//For env File 
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 3006;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cors())
+connect().then(() => {
+//   connectConsumers()
+}) 
+app.use(()=>{
+    console.log('Middleware')
+})
+
+// app.use('/', router)  
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Server is Fire at http://localhost:${port}`);
+});
