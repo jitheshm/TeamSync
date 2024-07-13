b9b1fa463bfd14dcf64926a1913a577961b75e6b
Jithesh M <jitheshmjithooz@gmail.com>
2024-07-13 10:22:23 +0530

    button updated

--- frontend/src/components/SubscriptionDetails/index.tsx
+++ frontend/src/components/SubscriptionDetails/index.tsx
@@ -41,7 +41,7 @@ function Index() {
             showCancelButton: true,
             confirmButtonColor: "#3085d6",
             cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
+            confirmButtonText: "Yes"
         }).then((result) => {
 
             if (result.isConfirmed) {
