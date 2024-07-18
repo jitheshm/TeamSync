429bad5b784d340b980f7cad2a1664e9fdc3fc4c
Jithesh M <jitheshmjithooz@gmail.com>
2024-07-19 00:34:47 +0530

    tickets table added in developers ui

--- frontend/src/components/TenantUserPanel/Tables/TicketTable.tsx
+++ frontend/src/components/TenantUserPanel/Tables/TicketTable.tsx
@@ -168,7 +168,9 @@ const TicketTable = ({ projectId, role, taskId }: { projectId: string, role: str
 
                                                 <option value="pending">pending</option>
                                                 <option value="resolved">resolved</option>
-                                                <option value="closed">closed</option>
+                                                {
+                                                    role === 'Tester' && <option value="closed">closed</option>
+                                                }
                                             </select>
                                         </div>
                                         <div className='flex col-span-3 space-x-2 justify-center'>
@@ -179,7 +181,10 @@ const TicketTable = ({ projectId, role, taskId }: { projectId: string, role: str
                                                         <Link href={`/employee/tester/dashboard/projects/${ticket.projects._id}/tasks/${ticket.tasks._id}/tickets/${ticket._id}/edit`} className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Edit</Link>
                                                         <button type="button" onClick={() => handleDelete(ticket._id)} className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</button>
                                                     </>) :
-                                                    ""
+                                                    <>
+                                                        <Link href={`/employee/developer/dashboard/projects/${ticket.projects._id}/tasks/${ticket.tasks._id}/tickets/${ticket._id}`} className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View</Link>
+
+                                                    </>
 
                                             }
                                         </div>
