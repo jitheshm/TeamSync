9988dca3b1b87c45471762d2c952d079df35019f
Jithesh M <jitheshmjithooz@gmail.com>
2024-07-27 13:57:40 +0530

    schedule feature added in remaining employee dashboard and fix some bugs in google login

---
+++ frontend/src/app/(tenantUser)/employee/manager/dashboard/meeting/schedule/page.tsx
@@ -0,0 +1,10 @@
+import ScheduleMeetingForm from '@/components/Meeting/Schedule'
+import React from 'react'
+
+function page() {
+    return (
+        <ScheduleMeetingForm Homeurl='/employee/manager/dashboard/meeting'/>
+    )
+}
+
+export default page
---
+++ frontend/src/app/(tenantUser)/employee/project_manager/dashboard/meeting/schedule/page.tsx
@@ -0,0 +1,10 @@
+import ScheduleMeetingForm from '@/components/Meeting/Schedule'
+import React from 'react'
+
+function page() {
+    return (
+        <ScheduleMeetingForm Homeurl='/employee/project_manager/dashboard/meeting'/>
+    )
+}
+
+export default page
