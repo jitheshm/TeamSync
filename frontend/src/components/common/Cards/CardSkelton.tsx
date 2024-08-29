65f6318944fbf1174d97757a33153ae97e25a98c
Jithesh M <jitheshmjithooz@gmail.com>
2024-08-29 21:04:35 +0530

    skelton design updated

--- frontend/src/components/common/Cards/CardSkelton.tsx
+++ frontend/src/components/common/Cards/CardSkelton.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 export function CardSkelton() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="  border border-border rounded-2xl  h-32 p-3  shadow-sm m-4 bg-gray-400/10  dark:bg-gray-600/10" />
+      <Skeleton className="  rounded-2xl  h-32 p-3  shadow-sm m-4 bg-gray-400/10  dark:bg-gray-600/10" />
     </div>
   )
 }
