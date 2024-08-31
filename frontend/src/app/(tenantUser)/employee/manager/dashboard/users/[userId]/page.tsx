c4d56c8072f50c96ef897e0e91d5e41b9316417d
Jithesh M <jitheshmjithooz@gmail.com>
2024-08-31 18:51:47 +0530

    tenant user details page design added

---
+++ frontend/src/app/(tenantUser)/employee/manager/dashboard/users/[userId]/page.tsx
@@ -0,0 +1,12 @@
+import TenantUserDetails from '@/components/common/TenantUserDetails'
+import React from 'react'
+
+function page() {
+    return (
+        <div className='md:p-10 p-5'>
+            <TenantUserDetails />
+        </div>
+    )
+}
+
+export default page
