5e908ee4275933811b2402daea2c580c790d4517
Jithesh M <jitheshmjithooz@gmail.com>
2024-09-12 23:41:35 +0530

    bug fixed

--- frontend/src/app/(tenantAdmin)/dashboard/users/[userId]/edit/page.tsx
+++ frontend/src/app/(tenantAdmin)/dashboard/users/[userId]/edit/page.tsx
@@ -1,11 +1,11 @@
-import UserForm from '@/components/common/Forms/UserForm';
+import AdminPanelUserForm from '@/components/common/Forms/AdminPanelUserForm';
 import React from 'react'
 
 const roles = [{ name: 'Manager', value: 'Manager' },{ name: 'Project_Manager', value: 'Project_Manager' }, { name: 'Developer', value: 'Developer' }, { name: 'Tester', value: 'Tester' }];
 
 function page({ params }: { params: { userId: string } }) {
     return (
-        <UserForm edit={true} userId={params.userId} roles={roles} />
+        <AdminPanelUserForm edit={true} userId={params.userId}  />
     )
 }
 
--- frontend/src/app/(tenantAdmin)/dashboard/users/register/page.tsx
+++ frontend/src/app/(tenantAdmin)/dashboard/users/register/page.tsx
@@ -1,11 +1,11 @@
-import UserForm from '@/components/common/Forms/UserForm';
+import AdminPanelUserForm from '@/components/common/Forms/AdminPanelUserForm';
 import React from 'react'
 
 const roles = [{ name: 'Manager', value: 'Manager' },{ name: 'Project_Manager', value: 'Project_Manager' }, { name: 'Developer', value: 'Developer' }, { name: 'Tester', value: 'Tester' }];
 
 function page() {
   return (
-    <UserForm roles={roles} />
+    <AdminPanelUserForm  />
    
   )
 }
