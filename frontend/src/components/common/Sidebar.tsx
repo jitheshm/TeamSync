db9466b6f1206f60ee779414d993f81a6c63a7d8
Jithesh M <jitheshmjithooz@gmail.com>
2024-09-13 00:01:38 +0530

    logout button added in new sidebar

--- frontend/src/components/common/Sidebar.tsx
+++ frontend/src/components/common/Sidebar.tsx
@@ -2,11 +2,28 @@
 import Image from 'next/image'
 import React from 'react'
 import MenuButton from './Buttons/MenuButton'
+import { CiLogout } from "react-icons/ci";
 
 import { IconType } from 'react-icons/lib';
 import { Icon } from '@/interfaces/Icon';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '../ui/tooltip';
+import { logout } from '@/features/user/userSlice';
+import { logout as adminLogout } from '@/features/admin/adminSlice';
+import { useDispatch } from 'react-redux';
+import Cookies from 'js-cookie'
 
 function Sidebar({ icons }: { icons: Icon[] }) {
+
+    const dispatch=useDispatch()
+
+    const handleLogout = () => {
+        dispatch(logout())
+        dispatch(adminLogout())
+        Cookies.remove('team-sync-token')
+        localStorage.removeItem('team-sync-refresh-token');
+
+    }
+
     return (
         <div className='h-screen border-r border-border w-20 pt-4 shadow-sm'>
             <div className='w-8 h-8 mx-auto '>
@@ -18,16 +35,27 @@ function Sidebar({ icons }: { icons: Icon[] }) {
                     icons.map((ele) => {
                         return (
                             <div key={ele.name} className='mx-auto w-11 h-11 my-4'>
-                                <MenuButton Icon={ele.icon}  tooltip={ele.name} link={ele.link}/>
+                                <MenuButton Icon={ele.icon} tooltip={ele.name} link={ele.link} />
                             </div>
                         )
                     })
                 }
-               
-                {/* <div className='mx-auto w-11 h-11 my-2'>
-                    <MenuButton Icon={LuUsers} bgColor='bg-selectionbackground' textColor='text-selectiontext' tooltip='Dashboard' />
-                </div> */}
-                
+
+                <div className='mx-auto w-11 h-11 my-2'>
+                    <TooltipProvider>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <div className={`w-full h-full flex justify-center items-center rounded-xl  `} onClick={handleLogout}>
+                                    <CiLogout/>
+                                </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                <p>Logout</p>
+                            </TooltipContent>
+                        </Tooltip>
+                    </TooltipProvider>
+                </div>
+
             </div>
 
         </div>
